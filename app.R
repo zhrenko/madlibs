372903b790c9ff5d163299b81139f35591f778f4
Zach Hrenko <zhrenko@cornerstone.com>
2025-09-16 12:34:26 -0600

    stringr :(

--- app.R
+++ app.R
@@ -1,12 +1,10 @@
 library(shiny)
-library(stringr)
 
 logging_message <- function(message) {
   cat(paste0(strrep("-", 20), "LOG: ", message))
   return(NULL)
 }
 
-logging_message("Loading script...")
 
 generate_story <- function(noun, verb, adjective, adverb) {
   logging_message("Generating story...")
